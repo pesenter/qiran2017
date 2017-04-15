44225e738ebe26c393393da2b7f85ea5b91e1123
pesenter <1655678396@qq.com>
2017-04-15 14:00:43 +0800

    asdfasdf Signed-off-by: pesenter <1655678396@qq.com>

---
+++ src/net/lifangyu/cheer/homework/ch01/Subject01.java
@@ -0,0 +1,10 @@
+package net.lifangyu.cheer.homework.ch01;
+
+public class Subject01 {
+
+	public static void main(String[] args) {
+		System.out.println("adasdfasdf");
+		System.out.println("asdfasfdads");
+	}
+
+}
