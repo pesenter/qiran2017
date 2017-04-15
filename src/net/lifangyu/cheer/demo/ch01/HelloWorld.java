97168aad03000673f4e7a7f0360c25bed0d07f13
pesenter <1655678396@qq.com>
2017-04-15 13:47:11 +0800

    Hello world demo

---
+++ src/net/lifangyu/cheer/demo/ch01/HelloWorld.java
@@ -0,0 +1,11 @@
+package net.lifangyu.cheer.demo.ch01;
+
+import java.util.Date;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World." + new Date());
+	}
+
+}
